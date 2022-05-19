85b0bda04e409506a1291450c958e225f6c196f8
lucassss-li <lucas8qwer@gmail.com>
2022-05-19 17:46:51 +0800

    [advance][fix][entry选项处理]

--- src/EntryPlugin.js
+++ src/EntryPlugin.js
@@ -6,6 +6,7 @@ class EntryPlugin {
         this.options = options || ''
     }
     apply(compiler) {
+        // STEP:给compilation挂载对应的模块工厂
         compiler.hooks.compilation.tap(
             'EntryPlugin',
             (compilation, { normalModuleFactory }) => {
@@ -15,10 +16,10 @@ class EntryPlugin {
                 )
             },
         )
-
+        // STEP:初始化入口依赖
         const { entry, options, context } = this
         const dep = EntryPlugin.createDependency(entry, options)
-
+        // STEP:进入make阶段时，向compilation中添加入口，开始编译流程
         compiler.hooks.make.tapAsync('EntryPlugin', (compilation, callback) => {
             compilation.addEntry(context, dep, options, err => {
                 callback(err)
--- src/dependencies/Dependency.js
+++ src/dependencies/Dependency.js
@@ -1,6 +1,12 @@
 class Dependency {
     constructor() {
         this._parentModule = undefined
+        this._locSL = 0
+        this._locSC = 0
+        this._locEL = 0
+        this._locEC = 0
+        this._locI = undefined
+        this._locN = undefined
         this._loc = undefined
     }
     get type() {
@@ -8,6 +14,62 @@ class Dependency {
     }
     get category() {
         return 'unknown'
+    }
+    get loc() {
+        if (this._loc !== undefined) return this._loc
+        /** @type {SyntheticDependencyLocation & RealDependencyLocation} */
+        const loc = {}
+        if (this._locSL > 0) {
+            loc.start = { line: this._locSL, column: this._locSC }
+        }
+        if (this._locEL > 0) {
+            loc.end = { line: this._locEL, column: this._locEC }
+        }
+        if (this._locN !== undefined) {
+            loc.name = this._locN
+        }
+        if (this._locI !== undefined) {
+            loc.index = this._locI
+        }
+        return (this._loc = loc)
+    }
+
+    set loc(loc) {
+        if ('start' in loc && typeof loc.start === 'object') {
+            this._locSL = loc.start.line || 0
+            this._locSC = loc.start.column || 0
+        } else {
+            this._locSL = 0
+            this._locSC = 0
+        }
+        if ('end' in loc && typeof loc.end === 'object') {
+            this._locEL = loc.end.line || 0
+            this._locEC = loc.end.column || 0
+        } else {
+            this._locEL = 0
+            this._locEC = 0
+        }
+        if ('index' in loc) {
+            this._locI = loc.index
+        } else {
+            this._locI = undefined
+        }
+        if ('name' in loc) {
+            this._locN = loc.name
+        } else {
+            this._locN = undefined
+        }
+        this._loc = loc
+    }
+
+    setLoc(startLine, startColumn, endLine, endColumn) {
+        this._locSL = startLine
+        this._locSC = startColumn
+        this._locEL = endLine
+        this._locEC = endColumn
+        this._locI = undefined
+        this._locN = undefined
+        this._loc = undefined
     }
 }
 
